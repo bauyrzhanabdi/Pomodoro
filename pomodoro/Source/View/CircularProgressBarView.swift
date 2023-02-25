34dd4cfb06f032d780232b07b2f411d524f0e2bc
Bauyrzhan Abdi <bauyrzhan.abdi@nu.edu.kz>
2023-02-25 22:10:52 +0600

    added progress bar

---
+++ pomodoro/Source/View/CircularProgressBarView.swift
@@ -0,0 +1,85 @@
+import UIKit
+
+final class CircularProgressBar: UIView {
+    // MARK: - Properties
+    private lazy var progressLayer: CAShapeLayer = CAShapeLayer()
+    private lazy var backgroundLayer: CAShapeLayer = CAShapeLayer()
+    private lazy var startPoint: CGFloat = CGFloat(-Double.pi / 2)
+    private lazy var endPoint: CGFloat = CGFloat(3 * Double.pi / 2)
+    private lazy var progress: CGFloat = 0 {
+        didSet {
+            progressLayer.strokeEnd = progress
+        }
+    }
+    
+    
+    // MARK: Initialization
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        createCircularPath()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Methods
+    func createCircularPath() {
+        let circularPath = UIBezierPath(
+            arcCenter: CGPoint(x: frame.size.width / 2, y: frame.size.height / 2),
+            radius: 150,
+            startAngle: startPoint,
+            endAngle: endPoint,
+            clockwise: true
+        )
+        
+        backgroundLayer.path = circularPath.cgPath
+        backgroundLayer.fillColor = UIColor.clear.cgColor
+        backgroundLayer.lineCap = .round
+        backgroundLayer.lineWidth = 20.0
+        backgroundLayer.strokeEnd = 1.0
+        backgroundLayer.strokeColor = UIColor.systemGray4.cgColor
+        
+        progressLayer.path = circularPath.cgPath
+        progressLayer.fillColor = UIColor.clear.cgColor
+        progressLayer.lineCap = .round
+        progressLayer.lineWidth = 10.0
+        progressLayer.strokeEnd = 0
+        progressLayer.strokeColor = UIColor.red.cgColor
+        
+        layer.addSublayer(backgroundLayer)
+        layer.addSublayer(progressLayer)
+    }
+    
+    func changeProgressBarColor(state: Bool, relaxingColor: UIColor, workingColor: UIColor) {
+        progressLayer.strokeColor = !state ? relaxingColor.cgColor : workingColor.cgColor
+    }
+    
+    func animateProgress(duration: TimeInterval) {
+        let animation = CABasicAnimation(keyPath: "strokeEnd")
+        
+        animation.duration = duration
+        animation.fromValue = 0
+        animation.toValue = 1.0
+        animation.fillMode = .forwards
+        animation.isRemovedOnCompletion = false
+        
+        progressLayer.add(animation, forKey: "progressAnimation")
+    }
+    
+    func pauseProgress() {
+        let pausedTime = progressLayer.convertTime(CACurrentMediaTime(), from: nil)
+        progressLayer.speed = 0.0
+        progressLayer.timeOffset = pausedTime
+    }
+
+    func resumeProgress() {
+        let pausedTime = progressLayer.timeOffset
+        progressLayer.speed = 1.0
+        progressLayer.timeOffset = 0.0
+        progressLayer.beginTime = 0.0
+        
+        let timeSincePause = progressLayer.convertTime(CACurrentMediaTime(), from: nil) - pausedTime
+        progressLayer.beginTime = timeSincePause
+    }
+}
--- pomodoro/Source/ViewController/PomodoroViewController.swift
+++ pomodoro/Source/ViewController/PomodoroViewController.swift
@@ -1,29 +1,41 @@
 import UIKit
 import SnapKit
 
-class PomodoroViewController: UIViewController {
-    // MARK: - Parameters
+final class PomodoroViewController: UIViewController {
+    // MARK: - Properties
     private lazy var isWorkTime: Bool = true
     private lazy var isStarted: Bool = false
+    private lazy var isActive: Bool = false
+    private lazy var workingColor: UIColor = .red
+    private lazy var relaxingColor: UIColor = .orange
     private lazy var timer: Timer? = nil
-    private var timeInSeconds: Int = 25
+    private lazy var milliseconds: TimeInterval = 0
+    private lazy var timeRemaining: TimeInterval = 25
+    private let totalTime: TimeInterval = 25
+    
     
     // MARK: - Outlets
     private lazy var countdownLabel: UILabel = UILabel(
-        text: get(time: timeInSeconds),
+        text: get(time: timeRemaining),
         allignment: .center,
-        color: isWorkTime ? .red : .green,
+        color: isWorkTime ? workingColor : relaxingColor,
         fontSize: 50,
         fontWeight: 0
     )
     
     private lazy var timerControlButton: UIButton = UIButton(
         image: isStarted ? "pause" : "play",
-        imageColor: isWorkTime ? .red : .green,
+        imageColor: isWorkTime ? workingColor : relaxingColor,
         backgroundColor: .clear,
         action: #selector(timerControlButtonPressed),
         target: self
     )
+    
+    private lazy var circularProgressBar: CircularProgressBar = {
+        let circularProgressBar = CircularProgressBar()
+        circularProgressBar.center = view.center
+        return circularProgressBar
+    }()
     
     // MARK: - Lifecycle
     override func viewDidLoad() {
@@ -35,6 +47,7 @@ class PomodoroViewController: UIViewController {
     
     // MARK: - Setup
     private func setupHierarchy() {
+        view.addSubview(circularProgressBar)
         view.addSubview(countdownLabel)
         view.addSubview(timerControlButton)
     }
@@ -51,13 +64,12 @@ class PomodoroViewController: UIViewController {
             make.width.equalTo(50)
             make.height.equalTo(50)
         }
-        
     }
     
     // MARK: - Methods
     private func startTimer() {
         timer = Timer.scheduledTimer(
-            timeInterval: 1.0,
+            timeInterval: 0.001,
             target: self,
             selector: #selector(updateTime),
             userInfo: nil,
@@ -76,7 +88,7 @@ class PomodoroViewController: UIViewController {
     }
     
     private func endTimer() {
-        if timeInSeconds == 0 {
+        if timeRemaining == 0 {
             timer?.invalidate()
             timer = nil
             changeState()
@@ -86,21 +98,27 @@ class PomodoroViewController: UIViewController {
     private func changeState() {
         isWorkTime.toggle()
         isStarted.toggle()
+        isActive.toggle()
         let image = isStarted ? "pause" : "play"
         timerControlButton.setImage(UIImage(systemName: image), for: .normal)
-        timeInSeconds = !isWorkTime ? 5 : 25
-        !isWorkTime ? changeVisual(color: .green) : changeVisual(color: .red)
+        timeRemaining = !isWorkTime ? 5 : 25
+        !isWorkTime ? changeVisual(color: relaxingColor) : changeVisual(color: workingColor)
     }
     
     private func changeVisual(color: UIColor) {
-        countdownLabel.text = get(time: timeInSeconds)
+        countdownLabel.text = get(time: timeRemaining)
         countdownLabel.textColor = color
         timerControlButton.tintColor = color
+        circularProgressBar.changeProgressBarColor(
+            state: isWorkTime,
+            relaxingColor: relaxingColor,
+            workingColor: workingColor
+        )
     }
     
-    private func get(time: Int) -> String {
-        let minutes = time / 60
-        let seconds = time % 60
+    private func get(time: TimeInterval) -> String {
+        let minutes = Int(time.rounded()) / 60
+        let seconds = Int(time.rounded()) % 60
         return String(format: "%02d:%02d", minutes, seconds)
     }
 
@@ -110,11 +128,22 @@ class PomodoroViewController: UIViewController {
         isStarted ? startTimer() : pauseTimer()
         let image = isStarted ? "pause" : "play"
         sender.setImage(UIImage(systemName: image), for: .normal)
+        if !isActive {
+            circularProgressBar.animateProgress(duration: timeRemaining)
+            isActive.toggle()
+        }
+        !isStarted ? circularProgressBar.pauseProgress() : circularProgressBar.resumeProgress()
     }
     
     @objc private func updateTime() {
-        timeInSeconds -= 1
-        countdownLabel.text = get(time: timeInSeconds)
+        if milliseconds < 1000 {
+            milliseconds += 1
+            return
+        }
+        
+        timeRemaining -= 1
+        milliseconds = 0
+        countdownLabel.text = get(time: timeRemaining)
         endTimer()
     }
     
