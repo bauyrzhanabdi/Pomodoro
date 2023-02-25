51f349b5bea7daf91b88f33247892e4feda97d1f
Bauyrzhan Abdi <bauyrzhan.abdi@nu.edu.kz>
2023-02-25 15:40:25 +0600

    added control button

--- pomodoro/Source/ViewController/PomodoroViewController.swift
+++ pomodoro/Source/ViewController/PomodoroViewController.swift
@@ -6,12 +6,20 @@ class PomodoroViewController: UIViewController {
     private lazy var isWorkTime: Bool = true
     private lazy var isStarted: Bool = false
     
-    private lazy var timerLabel: UILabel = UILabel(
+    private lazy var timerCountdownLabel: UILabel = UILabel(
         text: "25:00",
         allignment: .center,
         color: isWorkTime ? .red : .green,
         fontSize: 50,
         fontWeight: 0
+    )
+    
+    private lazy var timerControlButton: UIButton = UIButton(
+        image: isStarted ? "pause" : "play",
+        imageColor: isWorkTime ? .red : .green,
+        backgroundColor: .clear,
+        action: #selector(timerControlButtonPressed),
+        target: self
     )
     
     // MARK: - Lifecycle
@@ -24,18 +32,31 @@ class PomodoroViewController: UIViewController {
     
     // MARK: - Setup
     private func setupHierarchy() {
-        view.addSubview(timerLabel)
+        view.addSubview(timerCountdownLabel)
+        view.addSubview(timerControlButton)
     }
     
     private func setupLayout() {
-        timerLabel.snp.makeConstraints { make in
+        timerCountdownLabel.snp.makeConstraints { make in
             make.centerX.equalTo(view)
             make.centerY.equalTo(view)
         }
+        
+        timerControlButton.snp.makeConstraints { make in
+            make.centerX.equalTo(view)
+            make.top.equalTo(timerCountdownLabel.snp.bottom).offset(20)
+            make.width.equalTo(50)
+            make.height.equalTo(50)
+        }
+        
     }
     
     // MARK: - Actions
-    
+    @objc private func timerControlButtonPressed(_ sender: UIButton) {
+        isStarted.toggle()
+        let image = isStarted ? "pause" : "play"
+        sender.setImage(UIImage(systemName: image), for: .normal)
+    }
     
 }
 
